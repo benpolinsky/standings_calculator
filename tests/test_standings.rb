106cedc89fa102f2e66500eafc27e8b251811ed2
Ben Polinsky <benjamin.polinsky@gmail.com>
2017-05-03 17:20:17 -0400

    test output and file output

--- tests/test_standings.rb
+++ tests/test_standings.rb
@@ -38,13 +38,13 @@ class TestStandings < Minitest::Test
     assert_equal(ordered_hash, @standings.ordered)
   end
   
-  
-  def test_output
-  end
-  
   # So, this seems a bit clunky.
-  # I should have the output be returned to be printed rather than
+  # I should have the output returned to be printed rather than
   # printing from within the method
+  #
+  # It's because you tried to get clever with #each_info
+  # accepting a block :)
+  
   def test_to_s_with_no_block
     expected_output = ["\"1. My Fun Team, 6 pts\"\n\"2. Another Team, 3 pts\"\n\"2. My Other Team, 3 pts\"\n\"4. Trying Team, 0 pts\"\n", ""]
     out = capture_io do
@@ -54,9 +54,24 @@ class TestStandings < Minitest::Test
     assert_equal(expected_output, out)
   end
 
+  # should mock file, or at least mock the constant that sets the file name
   def test_print_to_file
-    File.delete('./output-file.txt')
-    assert_equal(File.exists?('./output-file.txt'), false)
+    File.delete('output-file.txt') if File.exists?('output-file.txt')
+    assert_equal(File.exists?('output-file.txt'), false)
+    @standings.print_to_file
+    assert_equal(File.exists?('output-file.txt'), true)
+  end
+  
+  def test_print_correct_standings_to_file
+    output = 
+    "1. My Fun Team, 6 pts
+2. Another Team, 3 pts
+2. My Other Team, 3 pts
+4. Trying Team, 0 pts
+"
+    File.delete('output-file.txt') if File.exists?('output-file.txt')
+    @standings.print_to_file
+    assert_equal(File.read('output-file.txt'), output)
   end
 
   # TODO: test or get rid of your block craziness :)
